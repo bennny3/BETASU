a5859ae55fb0551498d0e6ac7311c9970815804c
bennny3 <75240393+bennny3@users.noreply.github.com>
2020-12-06 10:06:05 +0700

    BETASU WEBSITE

---
+++ BETASU WEBSITE/script.js
@@ -0,0 +1,13 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "50%";
+    document.getElementById("mySidenav").style.opacity = "100%";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.8)";
+
+  }
+
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.opacity = "0%";
+    document.body.style.backgroundColor = "white";
+  }
+
